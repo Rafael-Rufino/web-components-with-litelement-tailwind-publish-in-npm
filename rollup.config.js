63abe9fd13810440bcc744a0a107b62653a4b8ed
rafael-rufino <rafaelrufino.52@gmail.com>
2022-09-21 12:24:21 -0300

    refactor: suindo modificação para publicar a lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,24 @@
-import typescript from 'rollup-plugin-typescript2';
-import del from 'rollup-plugin-delete';
-import pkg from './package.json';
+/* eslint-disable import/no-anonymous-default-export */
+import typescript from "rollup-plugin-typescript2";
+import del from "rollup-plugin-delete";
+import pkg from "./package.json";
+import postcss from "rollup-plugin-postcss"; // import postcss plugin
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/components/index.ts",
     output: [
       {
-        file: 'playground/src/component-lib/index.js',
-        format: 'esm',
-        banner: '/* eslint-disable */',
+        file: "playground/src/component-lib/index.js",
+        format: "esm",
+        banner: "/* eslint-disable */",
       },
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'esm' },
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "esm" },
     ],
     plugins: [
-      del({ targets: ['dist/*', 'playground/src/component-lib'] }),
+      postcss(), // use postcss plugin
+      del({ targets: ["dist/*", "playground/src/component-lib"] }),
       typescript(),
     ],
     external: Object.keys(pkg.peerDependencies || {}),
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { Button } from "./button";
+import { CheckText } from "./checkText";
+import { Heading } from "./heading";
+import { Input } from "./input";
+import { LinkIcon } from "./linkIcon";
+
+export { Button, CheckText, Heading, Input, LinkIcon };
