b5a45c1ccef28f52fe8bd743720dd61d62665808
rafael-rufino <rafaelrufino.52@gmail.com>
2022-09-21 02:13:46 -0300

    refactor: ajuste tecnico

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,60 @@
-import { createComponent } from '@lit-labs/react'
-import React from 'react'
+/* eslint-disable no-lone-blocks */
+import { createComponent } from "@lit-labs/react";
+import React from "react";
 
-import './App.css'
+import "./App.css";
 
-import { Heading } from './components/heading'
-import { Input } from './components/input'
-import { Button } from './components/button'
-import { LinkIcon } from './components/linkIcon'
-import { CheckText } from './components/checkText'
+import { Heading } from "./components/heading";
+import { Input } from "./components/input";
+import { Button } from "./components/button";
+import { LinkIcon } from "./components/linkIcon";
+import { CheckText } from "./components/checkText";
 
-import LockImg from './assets/lock.svg'
+import LockImg from "./assets/lock.svg";
 
-const HeadingApp = createComponent(React, 'heading-app', Heading)
-const InputApp = createComponent(React, 'input-app', Input)
-const ButtonApp = createComponent(React, 'button-app', Button)
-const LinkIconApp = createComponent(React, 'linkicon-app', LinkIcon)
-const CkeckTextApp= createComponent(React, 'checktext-app', CheckText)
+const HeadingApp = createComponent(React, "heading-app", Heading);
+const InputApp = createComponent(React, "input-app", Input);
+const ButtonApp = createComponent(React, "button-app", Button);
+const LinkIconApp = createComponent(React, "linkicon-app", LinkIcon);
+const CkeckTextApp = createComponent(React, "checktext-app", CheckText);
 
 function App() {
   return (
     <div className="App">
       <div className="Content">
-        <HeadingApp title="Acessando sua conta" description='Informe seu login e senha para continuar' />
-        <InputApp label='Seu login' placeholder="E-mail, CPF, Usuário ou Celular" type='text'  onChange={() => {}}/>
-        <InputApp label='Sua senha' placeholder="Digite a sua senha"  type='password' onChange={() => {}} />
-        <CkeckTextApp name='Manter conectado' />
-        <ButtonApp type='submit' name='Acessar sua conta' onClick={()=>{}}/>
-        <LinkIconApp  src={LockImg} title="Você deseja redefinir a senha ?" href="https://www.gazin.com.br"  name='Esqueceu sua senha?'/>
+        <HeadingApp
+          title="Acessando sua conta"
+          description="Informe seu login e senha para continuar"
+        />
+        <InputApp
+          label="Seu login"
+          placeholder="E-mail, CPF, Usuário ou Celular"
+          type="text"
+          onChange={() => {}}
+        />
+        <InputApp
+          label="Sua senha"
+          placeholder="Digite a sua senha"
+          type="password"
+          onChange={() => {}}
+        />
+        <CkeckTextApp name="Manter conectado" />
+        <ButtonApp type="submit" name="Acessar sua conta" onClick={() => {}} />
+        <LinkIconApp
+          src={LockImg}
+          title="Você deseja redefinir a senha ?"
+          href="https://www.gazin.com.br"
+          name="Esqueceu sua senha?"
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
-{/* <PeopleAppA
+{
+  /* <PeopleAppA
   props={[
     {
       name: 'Person 1',
@@ -44,4 +64,5 @@ export default App
       color: '84b22f',
     },
   ]}
-/> */}
+/> */
+}
